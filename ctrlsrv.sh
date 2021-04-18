2e32e18fce35618661d711f37a14db023d14e179
frfuan2021asix2 <80913548+frfuan2021asix2@users.noreply.github.com>
2021-04-18 18:49:57 +0200

    Add files via upload

---
+++ ctrlsrv.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#ctrlsrv.sh
+mostra_serveis () { 
+
+clear
+local opcion
+echo " Escoja una opcion "
+echo "1. apache"
+echo "2. cups"
+echo "3. ssh"
+echo "4. Salir"
+echo -n "Seleccione una opcion [1 - 4]"
+
+read opcion
+
+cntrl_serveis $opcion
+}
+
+cntrl_serveis () {
+
+case $1 in 
+    1) 
+        echo "Apache2";
+        systemctl start apache2
+        exit 2
+        ;;
+    2)
+        echo "CUPS";
+        systemctl start cups
+        ;;
+    3)
+        echo "SSH";
+        systemctl start ssh
+        ;;
+    4)
+        echo "chao";
+        ;;
+    *) 
+        echo "$opc es una opcion invalida. Es tan dificil?";
+        ;;
+
+esac
+
+}
+
+clear
+if (( EUID != 0 ))
+then
+  echo "Ejecutalo con root"
+  exit 9
+fi
+
+mostra_serveis 
